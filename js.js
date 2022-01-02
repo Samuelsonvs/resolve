39e5f1e0e905c8f8ac3cbbd31a445a43b7dbb4c8
Samuelsonvs <m.sametatali@gmail.com>
2022-01-02 18:09:29 +0300

    Duplicates handle

--- js.js
+++ js.js
@@ -111,7 +111,7 @@ const UploadImage = ({ handleUploadImages = () => {}, products }) => {
     let imagesDuplicate = []
 
     const newFilteredImages = images.filter(newImg => !existingImgNames.includes(newImg.imageFileName) ? newImg : imagesDuplicate.push(newImg));
-    imagesDuplicate.length > 0 && setDuplicateImages(existingImgNames);
+    imagesDuplicate.length > 0 && setDuplicateImages(imagesDuplicate);
     existingImgNames.length > 0 ? (setIsOpen(true), setNewImages(newFilteredImages)) : (handleUploadImages(images));
   };
 
